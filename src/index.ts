4f4c6df609b67f2f324f109cbe60ac97e2a713d9
Karina-Moveo <karinab@moveo.co.il>
2022-06-19 16:02:49 +0300

    Teams added to leagues

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as _ from 'lodash'
 import '../public/static/css/style.scss'
-import { getLeaguesNames } from "./services/leagues"
+import { Iteam, getLeaguesNames, getTeamsInLeague } from "./services/leagues"
 
 const createTabs: any = async () => {
     const leagueNames = await getLeaguesNames()
@@ -13,13 +13,41 @@ const createTabs: any = async () => {
         const listItem = document.createElement('li')
         const btn = document.createElement('button')
         btn.tabIndex = leagueNames.indexOf(element)
+        btn.id = element
         btn.textContent = element
+        btn.addEventListener('click', toggleTab)
         listItem.appendChild(btn)
-        tabList.appendChild(listItem)    
+        tabList.appendChild(listItem)  
     });
 
     tabs.appendChild(tabList)
     document.body.appendChild(tabs)
 }
+
+const toggleTab = async (event: MouseEvent): Promise<void> => {
+    if (document.getElementsByClassName('teams-container')[0])
+        document.body.removeChild(document.getElementsByClassName('teams-container')[0])
+    const target = (event.target as HTMLButtonElement)
+    let teamsNames: Array<Iteam> = await getTeamsInLeague(target.innerText)
+    const div = document.createElement('div')
+    div.classList.add('teams-container')
+
+    teamsNames.forEach(team => {
+        const teamElem = document.createElement('p')
+        if (team.logo) {
+            const logo = document.createElement('img')
+            logo.classList.add('logo-container')
+            logo.src = team.logo
+            teamElem.appendChild(logo)
+        }
+        teamElem.innerHTML += team.name
+        div.appendChild(teamElem)
+    });
+
+    document.body.appendChild(div)
+    
+    console.log(teamsNames)
+}
+
 
 createTabs()
--- src/services/leagues.ts
+++ src/services/leagues.ts
@@ -1,5 +1,10 @@
 const theSportsDB = require('thesportsdb');
 
+export interface Iteam {
+    name: string,
+    logo: string    
+}
+
 export const getLeaguesNames = async (): Promise<Array<string>> => {
     let leagueNames: Array<string> = []
     theSportsDB.setApiKey(2)
@@ -8,5 +13,19 @@ export const getLeaguesNames = async (): Promise<Array<string>> => {
         leagueNames.push(element.strLeague)
     });
     return leagueNames
+}
+
+export const getTeamsInLeague = async (leagueName: string) : Promise<Array<Iteam>> => {
+    let teamsNames: Array<Iteam> = []
+    theSportsDB.setApiKey(2)
+    const teams = await theSportsDB.getTeamsByLeagueName(leagueName)
+    teams.teams.forEach((element: { strTeam: string; strTeamLogo: string; }) => {
+        let tempTeam: Iteam = {} as Iteam
+        tempTeam.name = element.strTeam
+        tempTeam.logo = element.strTeamLogo
+        teamsNames.push(tempTeam)
+    });
+    
+    return teamsNames
 }
 
